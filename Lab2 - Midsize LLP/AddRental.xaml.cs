2a3422e5b3ce5e7527a581ece7921437952982f1
Nick Sturch-Flint <nick.rulez@live.com>
2020-10-18 13:29:29 -0400

    AddCar User Control Finished - Let The Testing Begin!

--- Lab2 - Midsize LLP/AddRental.xaml.cs
+++ Lab2 - Midsize LLP/AddRental.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
 
 namespace Lab2___Midsize_LLP
 {
@@ -23,6 +24,49 @@ namespace Lab2___Midsize_LLP
         public AddRental()
         {
             InitializeComponent();
+        }
+        //Generates the ID
+        public int random()
+        {
+            Random rand = new Random();
+            int number = rand.Next();
+            return number;
+        }
+
+        private void addHardwareToDatabase(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                //Retrieve the database location
+                string connectString = Properties.Settings.Default.connection_string;
+                //Create a connection
+                SqlConnection conn = new SqlConnection(connectString);
+                //open the conn
+                conn.Open();
+
+                //generate a random ID
+                int ID = random();
+
+                //Insert Query
+                string insertQuery = "INSERT INTO Hardware (empID, name, desc, phone) VALUES('" + ID + "', '" + EmployeeName.Text + "', '" + ItemDescription.Text + "', '" + ContactNumber.Text + "')";
+                //create the command
+                SqlCommand command = new SqlCommand(insertQuery, conn);
+                //execute the query
+                command.ExecuteNonQuery();
+                //close the connection
+                conn.Close();
+
+                //Show a Popup Message if the record is added successfully
+                MessageBox.Show("Congrats! You have Rented That Hardware Item!");
+                //Empty the text boxes
+                EmployeeName.Text = string.Empty;
+                ItemDescription.Text = string.Empty;
+                ContactNumber.Text = string.Empty;
+            }
+            catch  (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
         }
     }
 }
